4d98f2adcb47d31a5a255814e02d1420a896d77a
XiaoLaoHus <2576381123@qq.com>
2024-09-14 23:54:42 +0800

    Site updated: 2024-09-14 23:54:42

--- js/fomal.js
+++ js/fomal.js
@@ -1320,22 +1320,22 @@ function share() {
 
 /* 恶搞标题 start */
 //动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
+//var OriginTitile = document.title;
+//var titleTime;
+//document.addEventListener('visibilitychange', function () {
+//  if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
-    clearTimeout(titleTime);
-  } else {
+//    document.title = '👀跑哪里去了~';
+//    clearTimeout(titleTime);
+//  } else {
     //返回当前页面时标签显示内容
-    document.title = '😅抓到你啦～';
+//    document.title = '😅抓到你啦～';
     //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+//    titleTime = setTimeout(function () {
+//      document.title = OriginTitile;
+//    }, 2000);
+//  }
+//});
 /* 恶搞标题 end */
 
 //----------------------------------------------------------------
